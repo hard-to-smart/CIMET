be3d4491b504d13acbc755666563640623646f14
Divya Punjabi <116953971+hard-to-smart@users.noreply.github.com>
2024-10-08 12:49:04 +0530

    Update Carousel.jsx

--- React JavaScript/Unsplash/unsplash/src/components/Carousel.jsx
+++ React JavaScript/Unsplash/unsplash/src/components/Carousel.jsx
@@ -1,10 +1,11 @@
 import React, { useEffect, useState } from "react";
 import { IoCaretBackOutline } from "react-icons/io5";
 import { IoCaretForwardOutline } from "react-icons/io5";
-import Spinner from "./Spinner";
+import Spinner from "../Spinner";
 
 const Carousel = ({ images }) => {
   const [currentIndex, setCurrentIndex] = useState(0);
+  const maxLength = images.length
   const handleIndexChange = (id) => {
     if (id === "prev") {
       setCurrentIndex((prev) => (prev === 0 ? images.length - 1 : prev - 1));
@@ -12,9 +13,10 @@ const Carousel = ({ images }) => {
       setCurrentIndex((prev) => (prev === images.length - 1 ? 0 : prev + 1));
     }
   };
+ 
 
   return (
-    <div className="flex flex-row w-full relative h-screen items-center justify-center gap-4">
+    <div className="flex flex-row relative h-[80vh] max-w-[80vw] items-center justify-center gap-4 overflow-hidden mx-40">
       <button
         id="prev"
         className="absolute w-[50px] h-[50px] flex items-center justify-center rounded-full left-10 bg-blue-400 focus:bg-blue-600 hover:bg-blue-600 hover:ring-offset-zinc-200 hover:ring-4 ring z-10"
@@ -23,22 +25,19 @@ const Carousel = ({ images }) => {
         <IoCaretBackOutline size={28} />
       </button>
     
-      <div className=" flex flex-row flex-wrap justify-center overflow-hidden h-[700px]">
-        {images.map((image, index) => {
-          return (
-            <div key={index} className="container inline-flex justify-center overflow-hidden w-full h-[80%] items-center objtect-cover gap-4"
-            style={{ transform: '-transitionX  '}} >
-              <img
-                id={image.id}
-                src={image.urls.small}
-                alt={image.alt_description}
-                onClick={() => console.log(index)}
-              />
-            </div>
-          );
-        })}
+      <div className='flex duration-300 h-full w-full' style={{ transform: ` ${currentIndex === maxLength ? 0 : `translateX(-${currentIndex * 100}%)`}` }}>
+        {images.map((image) => (
+          <div key={image.id} className="flex-shrink-0 flex w-full h-full items-center justify-center">
+            <img
+              src={image.urls.small}
+              alt={image.alt_description}
+              className="object-cover"
+              onClick={() => console.log(image.id)}
+            />
+          </div>
+        ))}
       </div>
-      <button
+     <button
         id="next"
         className="absolute  w-[50px] h-[50px] rounded-full text-center right-10 bg-blue-400 flex items-center focus:bg-blue-600 hover:bg-blue-600 hover:ring-offset-zinc-200 hover:ring-4 ring justify-center z-10"
         onClick={() => handleIndexChange("next")}
@@ -50,11 +49,3 @@ const Carousel = ({ images }) => {
 };
 
 export default Carousel;
-
-// <img
-// key={index}
-// id={image.id}
-// src={image.urls.small}
-// alt={image.alt_description}
-// onClick={() => console.log(index)}
-// />
